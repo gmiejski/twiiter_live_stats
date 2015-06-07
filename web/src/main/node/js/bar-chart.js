633aece6c3baa24aa12cc6314dd8677b929ceb0b
Piotr Góralczyk <piotr.a.goralczyk@gmail.com>
2015-06-07 17:43:04 +0200

    Create new bar chart and adjust live update logic

--- web/src/main/node/js/bar-chart.js
+++ web/src/main/node/js/bar-chart.js
@@ -1,153 +1,198 @@
 /**
  * Created by piotrek on 6/7/15.
  */
-function formatDate(picker) {
-    var date = getData(picker).date();
-    return date == null ? null : date.format('YYYY-MM-DD-HH:MM');
-}
-
-function getData(picker) {
-    return picker.data("DateTimePicker");
-}
-
-function draw(keywords, occurrences) {
-    var width = 800,
-        bar_height = 20,
-        height = bar_height * keywords.length,
-        left_width = 100,
-        gap = 2;
-
-    var x = d3.scale.linear()
-        .domain([0, d3.max(occurrences)])
-        .range([0, width]);
-
-    var y = d3.scale.ordinal()
-        .domain(occurrences)
-        .rangeBands([0, (bar_height + 2 * gap) * keywords.length]);
-
-    d3.select('#barchart').select('svg').remove();
-
-    var chart = d3.select('#barchart')
-        .append('svg')
-        .attr('class', 'chart')
-        .attr('width', left_width + width + 40)
-        .attr('height', (bar_height + gap * 2) * keywords.length + 30)
-        .append("g")
-        .attr("transform", "translate(10, 20)");
-
-    chart.selectAll("line")
-        .data(x.ticks(d3.max(occurrences)))
-        .enter().append("line")
-        .attr("x1", function (d) {
-            return x(d) + left_width;
-        })
-        .attr("x2", function (d) {
-            return x(d) + left_width;
-        })
-        .attr("y1", 0)
-        .attr("y2", (bar_height + gap * 2) * keywords.length);
-
-    chart.selectAll(".rule")
-        .data(x.ticks(d3.max(occurrences)))
-        .enter().append("text")
-        .attr("class", "rule")
-        .attr("x", function (d) {
-            return x(d) + left_width;
-        })
-        .attr("y", 0)
-        .attr("dy", -6)
-        .attr("text-anchor", "middle")
-        .attr("font-size", 10)
-        .text(String);
-
-    chart.selectAll("rect")
-        .data(occurrences)
-        .enter().append("rect")
-        .attr("x", left_width)
-        .attr("y", function (d) {
-            return y(d) + gap;
-        })
-        .attr("width", x)
-        .attr("height", bar_height);
-
-    chart.selectAll("text.score")
-        .data(occurrences)
-        .enter().append("text")
-        .attr("x", function (d) {
-            return x(d) + left_width;
-        })
-        .attr("y", function (d, i) {
-            return y(d) + y.rangeBand() / 2;
-        })
-        .attr("dx", -5)
-        .attr("dy", ".36em")
-        .attr("text-anchor", "end")
-        .attr('class', 'score')
-        .text(String);
-
-    chart.selectAll("text.name")
-        .data(keywords)
-        .enter().append("text")
-        .attr("x", left_width / 2)
-        .attr("y", function (d, i) {
-            return y(occurrences[i]) + y.rangeBand() / 2;
-        })
-        .attr("dy", ".36em")
-        .attr("text-anchor", "middle")
-        .attr('class', 'name')
-        .text(String);
-}
 
 $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var keywords = [];
+
     var startPicker = $('#datetimepicker-start');
     var endPicker = $('#datetimepicker-end');
     var options = {
         showClear: true
     };
-
     startPicker.datetimepicker(options);
     endPicker.datetimepicker(options);
     startPicker.on("dp.change", function (e) {
-        if (e.date != null) {
-            getData(endPicker).minDate(e.date);
-        }
+        getData(endPicker).minDate(getDateLimit(e.date));
     });
     endPicker.on("dp.change", function (e) {
-        if (e.date != null) {
-            getData(startPicker).maxDate(e.date)
-        }
+        getData(startPicker).maxDate(getDateLimit(e.date));
     });
 
     $("#reload-button").click(function () {
+        var startDate = formatDate(startPicker);
+        var endDate = formatDate(endPicker);
+
+        if (endDate == null) {
+            turnOnLiveUpdate();
+        } else {
+            turnOffLiveUpdate();
+        }
+
         $.get(
             "http://localhost:8080/keywords",
             {
-                startDate: formatDate(startPicker),
-                endDate: formatDate(endPicker)
+                startDate: startDate,
+                endDate: endDate
             },
             function (data) {
-                console.log(data);
+                sortKeywords(data);
+                keywords = data;
+                redrawBarChart();
             }
         );
     });
 
-    var keywords = [];
-    var occurrences = [];
+    $("#clear-button").click(function () {
+        keywords = [];
+        getData(startPicker).date(null);
+        getData(endPicker).date(null);
+        redrawBarChart();
+        turnOnLiveUpdate();
+    });
 
     var ws = new WebSocket('ws://localhost:8085/main');
+    turnOnLiveUpdate();
 
-    ws.onmessage = function (message) {
+    function getData(picker) {
+        return picker.data("DateTimePicker");
+    }
+
+    function getDateLimit(date) {
+        return date === null ? false : date;
+    }
+
+    function formatDate(picker) {
+        var date = getData(picker).date();
+        return date === null ? null : date.format('YYYY-MM-DD-HH:MM');
+    }
+
+    function sortKeywords(keywords) {
+        keywords.sort(function (a, b) {
+            var x = a.value;
+            var y = b.value;
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+
+    function getKeyword(keyword) {
+        return keyword.value;
+    }
+
+    function getFrequency(keyword) {
+        return keyword.occurrences.length;
+    }
+
+    function turnOnLiveUpdate() {
+        ws.onmessage = updateOccurrences;
+        $('#live-update-icon').show();
+    }
+
+    function turnOffLiveUpdate() {
+        ws.onmessage = doNothing;
+        $('#live-update-icon').hide();
+    }
+
+    function updateOccurrences(message) {
         console.log(message.data);
-        var matches = JSON.parse(message.data);
-        for (var i = 0; i < matches.length; i++) {
-            var keywordIndex = keywords.indexOf(matches[i]);
-            if (keywordIndex == -1) {
-                keywords.push(matches[i]);
-                occurrences.push(1);
+        var newOccurrences = JSON.parse(message.data);
+        $.each(newOccurrences, function (index, newOccurrence) {
+            var existingKeywords = $.grep(keywords, function (kw) {
+                return getKeyword(kw) === newOccurrence;
+            });
+            var existingKeywordsCount = existingKeywords.length;
+            var currentTimestamp = Date.now();
+            if (existingKeywordsCount == 1) {
+                existingKeywords[0].occurrences.push(currentTimestamp);
+            } else if (existingKeywordsCount == 0) {
+                keywords.push({value: newOccurrence, occurrences: [currentTimestamp]});
+                sortKeywords(keywords);
             } else {
-                occurrences[keywordIndex]++;
+                console.warn('WTF');
             }
-        }
-        draw(keywords, occurrences)
-    };
+        });
+        redrawBarChart();
+    }
+
+    function doNothing(message) {
+    }
+
+    function redrawBarChart() {
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10)
+            .tickFormat(d3.format("d"));
+
+        d3.select('#barchart').select('svg').remove();
+
+        var svg = d3.select("#barchart")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(keywords.map(getKeyword));
+        y.domain([0, d3.max(keywords, getFrequency)]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Frequency");
+
+        var bar = svg.selectAll(".bar")
+            .data(keywords)
+            .enter();
+
+        bar.append("rect")
+            .attr("class", "bar")
+            .attr("x", function (kw) {
+                return x(getKeyword(kw));
+            })
+            .attr("width", x.rangeBand())
+            .attr("y", function (kw) {
+                return y(getFrequency(kw));
+            })
+            .attr("height", function (kw) {
+                return height - y(getFrequency(kw));
+            });
+
+        bar.append("text")
+            .attr("class", "count")
+            .attr("x", function (kw) {
+                return x(getKeyword(kw)) + x.rangeBand() / 2;
+            })
+            .attr("y", function (kw) {
+                return y(getFrequency(kw)) - 10;
+            })
+            .attr("dy", ".35em")
+            .text(getFrequency);
+    }
 });

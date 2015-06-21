cfbc5a3b3240a0f8f232fdb78c9d0f298eb52d9c
Piotr Góralczyk <piotr.a.goralczyk@gmail.com>
2015-06-21 14:52:28 +0200

    Add world map to the page

--- web/src/main/node/js/common.js
+++ web/src/main/node/js/common.js
@@ -1,6 +1,3 @@
 /**
  * Created by piotrek on 6/20/15.
  */
-$(document).ready(function(){
-    $('.screen-height').height($(window).height() - 50);
-});
---
+++ web/src/main/node/js/world-map.js
@@ -0,0 +1,9 @@
+/**
+ * Created by piotrek on 6/21/15.
+ */
+
+$(function () {
+    $('.world-map').datamaps({
+        projection: 'mercator'
+    });
+});

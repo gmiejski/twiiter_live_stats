4be5f7f488f77d44437cbed7e044056379f5f18d
Piotr Góralczyk <piotr.a.goralczyk@gmail.com>
2015-06-21 20:22:34 +0200

    Clean up JS library files

--- web/src/main/node/server.js
+++ web/src/main/node/server.js
@@ -1,12 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic('./')).listen(8081);
-
-//var WebSocketServer = require('ws').Server
-//var wss = new WebSocketServer({port: 8085, path: '/main'});
-//wss.on('connection', function (ws) {
-//    ws.on('message', function (message) {
-//        console.log('received: %s', message);
-//    });
-//    ws.send('something');
-//});
